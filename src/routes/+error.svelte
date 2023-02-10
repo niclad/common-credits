c54ab52ceda1568dc3c9958e87da3c232c63c3bb
Nicolas <nicktedori@gmail.com>
2023-02-10 16:49:48 -0500

    create link dropdown copier

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="">
+	<h1 class="pg-error">{$page.status}</h1>
+	<h3 class="pg-error">{$page.error?.message}</h3>
+	<p class="mx-auto">Nothing to see here... Move along now.</p>
+</div>
+
+<style>
+	p {
+		font-weight: bold;
+		text-align: center;
+	}
+
+	.pg-error {
+		text-align: center;
+	}
+</style>
--- src/routes/SearchBar.svelte
+++ src/routes/SearchBar.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import axios from 'axios';
-	import { debug } from 'svelte/internal';
+  import { fade } from 'svelte/transition';
+	import { page } from '$app/stores';
 
 	// The requested media data
 	export let mediaData;
@@ -29,43 +30,65 @@
 	async function getMediaInfo() {
 		isSubmitted = true;
 
-    
 		// Check if it's being tested
 		if (selectedMedia.includes(undefined) || enteredId.includes(undefined)) {
-      selectedMedia = ['0', '0'];
+			selectedMedia = ['0', '0'];
 			enteredId = ['9718', '12133'];
 		}
-    
+
 		// Build the search parameters
 		let searchParams = [];
 		for (const id of enteredId) {
-      searchParams.push(['id', id]);
+			searchParams.push(['id', id]);
 		}
 		for (const mediaType of selectedMedia) {
-      searchParams.push(['type', mediaType]);
+			searchParams.push(['type', mediaType]);
 		}
 		const queryParams = new URLSearchParams(searchParams).toString();
-    
+
 		// Build the request URL
 		const url = `/api/tmdb?${queryParams}`;
-    
+
 		// Request the results
 		axios.get(url).then(
-      (response) => {
-        mediaData = response.data;
+			(response) => {
+				mediaData = response.data;
 				console.log('Successfully obtained media data.');
 
-        // Check if there's an error message and clear it
-        if (errorMessage) {
-          errorMessage = '';
-        }
+				// Check if there's an error message and clear it
+				if (errorMessage) {
+					errorMessage = '';
+				}
 			},
 			(error) => {
-        errorMessage = error.response.data.message;
+				errorMessage = error.response.data.message;
 				console.log(errorMessage);
 			}
-      );
+		);
 	}
+
+  let copyMsg: string;
+  let copySuccess: boolean = false;
+  let copyLink: string = $page.url.toString();
+
+  function copyToClipboard() {
+
+    // Actually copy the link to the clipboard
+    navigator.clipboard.writeText(copyLink).then(
+      () => {
+        console.log(`Copied ${copyLink} to clipboard!`);
+        copyMsg = 'Copied to clipboard!';
+        copySuccess = true;
+        setTimeout(() => {
+          copyMsg = '';
+          copySuccess = false;
+        }, 2000);
+      },
+      () => {
+        console.log('Failed to copy to clipboard! Highlight and copy manually.');
+      }
+    );
+  }
 </script>
 
 <!-- Search bars -->
@@ -101,14 +124,51 @@
 	{/each}
 </form>
 
-<!-- Go button -->
-<button
-	class="btn btn-primary mx-auto d-block"
-	type="submit"
-	on:click={() => getMediaInfo()}
-	disabled={(selectedMedia.includes(undefined) || enteredId.includes(undefined)) && false}
-	>{goBtnText}</button
->
+<div class="row justify-content-center">
+	<!-- Go button -->
+	<div class="col">
+		<button
+			class="btn btn-primary"
+			type="submit"
+			on:click={() => getMediaInfo()}
+			disabled={(selectedMedia.includes(undefined) || enteredId.includes(undefined)) && false}
+			>{goBtnText}</button
+		>
+	</div>
+	<!-- Share button -->
+	<div class="col">
+		<div class="dropdown">
+			<button
+				type="button"
+				class="btn btn-secondary"
+				data-bs-toggle="dropdown"
+				aria-expanded="false"
+				data-bs-auto-close="outside"
+			>
+				<i class="bi bi-share-fill" />
+			</button>
+			<div class="dropdown-menu p-1 share-menu">
+				<div class="d-flex flex-row justify-content-center align-items-center">
+					<!-- <div class="col"> -->
+						<div class="alert alert-dark user-select-all mb-0 overflow-auto text-nowrap flex-shrink-1 lh-1 link-box">{copyLink}</div>
+					<!-- </div> -->
+					<!-- <div class="col"> -->
+						<button class:btn-success={copySuccess} class="btn btn-primary btn-sm ms-1 flex-shrink-0" on:click={copyToClipboard}>
+              {#if copySuccess}
+                <i class="bi bi-check2" />
+              {:else}
+                <i class="bi bi-clipboard2-fill" />
+              {/if}
+              Copy</button>
+					<!-- </div> -->
+				</div>
+        {#if copyMsg && false}
+          <div class="badge text-bg-success mt-1 mb-0 text-center" out:fade>{copyMsg}</div>
+        {/if}
+			</div>
+		</div>
+	</div>
+</div>
 
 <!-- Error alert -->
 {#if errorMessage}
@@ -125,8 +185,16 @@
 		border-radius: 0.375rem 0 0 0.375rem !important;
 	}
 
-	button {
+	/* button {
 		width: 10%;
+	} */
+
+	.share-menu {
+		max-width: 400px;
+	}
+
+	.link-box {
+		padding: 0.6rem;
 	}
 
 	.id-entry {
